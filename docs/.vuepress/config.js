911a0713df7aa92febd95790c3dfc299dea3eacf
wowoqu <1024125388@qq.com>
2021-07-30 22:21:59 +0800

    处理页面默认配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,17 @@
 module.exports = {
     title: 'wowoqu的博客',
-    description: '救赎之道，就在其中',
-    head: [ // 注入到当前页面的 HTML <head> 中的标签
-        ['link', { rel: 'icon', href: '/images/logo/びじん1.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+    description: '技术不完全指北手册',
+    head: [
+        ['link', { rel: 'icon', href: '/images/logo/panda.png' }]
     ],
     themeConfig: {
-        logo: '/images/logo/びじん1.png',  // 导航logo
-        nav: [ // 导航栏配置
-            { text: '首页', link: '/' },  // 内链
-            { text: '技术文档', link: '/tech/interview/' },
-            { text: '简书主页', link: 'https://www.jianshu.com/u/c455567c7f50' },  // 外链
-            { text: 'External', link: 'https://google.com', target: '_self', rel: '' },  // 页面跳转行为
-            {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
-                items: [  // 列表
-                    { text: 'Chinese', link: '/language/chinese/' },
-                    { text: 'Japanese', link: '/language/japanese/' }
-                ]
-            },
-            {
-                text: 'Languages',
-                items: [  // 分组
-                    { text: 'Group1', items: [/*  */] },
-                    { text: 'Group2', items: [/*  */] }
-                ]
-            }
+        logo: '/images/logo/panda.png',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: 'Linux', link: '/linux/' },
+            { text: 'GitHub', link: 'https://github.com/wowoqu' }
         ],
-        sidebar: 'auto', // 侧边栏配置
+        sidebar: 'auto',
         lastUpdated: 'Last Updated', // 最后更新时间
         smoothScroll: true,  // 页面滚动
     }
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,31 @@
 ---
 home: true
 heroImage: /images/logo/びじん1.png
-heroText: 我的主页
-tagline: My homepage
-actionText: 开启旅程 →
-actionLink: /tech/interview/
+heroText: 不完全指北手册
+tagline: Make Code Cool Again!
+actionText: 立即入坑 →
+actionLink: /linux/
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: wowoqu的个人博客
+- title: 繁杂
+  details: 目标是能用两句话表示的，绝不用一句话表达。
+- title: 低效
+  details: 通过记录大量的无用信息，让用户花费更多的时间来找到自己想要的信息。
+- title: 坑爹
+  details: 内置多种纷繁复杂的技术文档，让用户眼花缭乱，从而实现离开后什么也没记住的忘我境界。
+footer: © wowoqu 京ICP备20028924号
 ---
+
+# 像数1，2，3 一样复杂
+```md
+# 开始
+随意打开一个笔记开启学习之旅
+
+# 学习
+打开编辑器，仿照笔记进行练习
+
+# 结束
+关闭网页，关闭编辑器
+
+# 最后
+重新打开本页，然后打爆作者的狗头
+```
